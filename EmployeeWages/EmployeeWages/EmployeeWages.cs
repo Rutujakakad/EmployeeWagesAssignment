5a2453f395099f69a23b647ae40191fdd8301169
67418_Rutuja <rutubkakad@gmail.com>
2024-12-24 14:42:43 +0530

    Solved the assignment

---
+++ EmployeeWages/EmployeeWages/EmployeeWages.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWages
+{
+    internal class EmployeeWages
+    {
+        public int FullTime = 1;
+        public int PartTime = 2;
+        public int wage_per_hr = 20;
+        public int full_day_hr = 8;
+        public int part_time_hr = 8;
+        public int working_days = 20;
+
+
+        public void Attendance()
+        {
+            Random rndm = new Random();
+            int attendance = rndm.Next(0, 3);
+
+
+            switch (attendance)
+            {
+                case 1: 
+                    Console.WriteLine($"Employee is Present: {FullTime}.");
+                    break;
+                case 2: 
+                    Console.WriteLine($"Employee is Present {PartTime}.");
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent.");
+                    break;
+            }
+        }
+        public void DailyEmployeeWage()
+        {
+            int DailyEmployeeWage = wage_per_hr * full_day_hr;
+            Console.WriteLine($"Daily Employee Wage is: {DailyEmployeeWage}");
+            
+        }
+        public void PartTimeEmployeeWage()
+        {
+            int PartTimeEmployeeWage = part_time_hr * wage_per_hr;
+            Console.WriteLine($"Part Time Employee Wage is: {PartTimeEmployeeWage}");
+        }
+
+        public void WagesPerMonth()
+        {
+            int WagesPerMonth = working_days * wage_per_hr * full_day_hr; 
+            Console.WriteLine($"Wages for a fulltime employee for a  month is: {WagesPerMonth}");
+        }
+
+        public void PartTimeWagesPerMonth()
+        {
+            int ParttimeForMonth = working_days * part_time_hr * wage_per_hr;
+            Console.WriteLine($"Wages for a parttime employee for a month is: {ParttimeForMonth}");
+        }
+    }
+}
---
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace EmployeeWages
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeWages emp = new EmployeeWages();
+            emp.Attendance();
+            emp.DailyEmployeeWage();
+            emp.PartTimeEmployeeWage();
+            emp.WagesPerMonth();
+            emp.PartTimeWagesPerMonth();
+        }
+    }
+}
